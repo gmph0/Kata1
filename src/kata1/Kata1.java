77f447e45e925a443d1ad5272bacf00de52aa927
Usuario <Usuario@10.22.148.84>
2018-09-13 13:55:06 +0100

    Revert "v2 de Kata1"
    This reverts commit 37244fad796bf8b3061803f2bd286bbc6d2c5c0c.

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,15 +1,11 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Date;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date;
-        date=GregorianCalendar.getInstance();
-        date.set(1992, 1, 6);
-        Person person = new Person("Paco", date);//6 enero 92
+        Person person = new Person("Paco", new Date(92,0,6));//6 enero 92
         System.out.print(person.getName() + " tiene ");
         System.out.println(person.getAge() + " años.");
     }
